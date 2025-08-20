fe9f0aa35e10c27d2b3d0fd295126cb6690bbd8c
Kevin Patel <patelkevin782002@gmail.com>
2025-08-20 20:30:44 +0530

    Expense Tracker

--- Frontend/Expense_Tracker/src/Components/Overview.jsx
+++ Frontend/Expense_Tracker/src/Components/Overview.jsx
@@ -1,5 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { EllipsisVertical , ChevronLeft , ChevronRight } from 'lucide-react';
+import api from '../utils/axiosInstance';
+import { EllipsisVertical, ChevronLeft, ChevronRight } from 'lucide-react';
+import { Bar } from 'react-chartjs-2'
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, plugins, scales } from 'chart.js'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
 
 import { path } from '../ContextAPI/path.context'
 
@@ -10,8 +16,15 @@ const Overview = () => {
   const [bank, setbank] = useState(0)
 
   // Transaction
-  const [transactionPopup , settransactionPopup] = useState(false)
-  const [itemShown , setitemShown] = useState(3)
+  const [transactionPopup, settransactionPopup] = useState(false)
+  const [itemShown, setitemShown] = useState(3)
+  const [tempitemShown , settempitemShown] = useState(itemShown)
+
+  // Cashflow
+  const [dates, setdates] = useState(new Date())
+
+  // Line Charts 
+  const [barChart, setbarChart] = useState({ labels: [], datasets: [] })
 
 
   // Overview-Accounts
@@ -23,11 +36,86 @@ const Overview = () => {
     setbank(transactions.reduce((sum, item) => item.account === 'Bank Account' ? sum + (item.amount) : sum, 0))
   }
 
+  // Cashflow
+
+  const handleleftarrow = () => {
+    setdates((prev) => {
+      const newDate = new Date(prev)
+      newDate.setMonth(newDate.getMonth() - 1)
+      return newDate
+    })
+  }
+
+  const handlerightarrow = () => {
+    setdates((prev) => {
+      const newDate = new Date(prev)
+      newDate.setMonth(newDate.getMonth() + 1)
+      return newDate
+    })
+  }
+
+  const formattedMonth = dates.toLocaleString('default', {
+    month: "long",
+    year: 'numeric'
+  })
+
+  const selectMonth = dates.getMonth()
+  const selectYear = dates.getFullYear()
+  const filteredTransaction = transactions.filter((item) => {
+    const itemDate = new Date(item.date)
+    const isSamemonth = itemDate.getMonth() === selectMonth && itemDate.getFullYear() === selectYear
+    return isSamemonth
+  })
+
+
+  // Bar Chart 
+
+  const barchart = async () => {
+    const bothTransaction = await api.get(`/expenses/both-chart-transaction`)
+    const expense = (Math.abs(bothTransaction.data.expense.reduce((sum, item) => (sum + (item.amount)), 0)))
+    const income = (bothTransaction.data.income.reduce((sum, item) => sum + (item.amount), 0))
+
+    setbarChart({
+      labels: ["Income", "Expense"],
+      datasets: [
+        {
+          data: [income, expense],
+          backgroundColor: [
+            "green", "red"
+          ]
+        }
+      ]
+    })
+  }
+
+  const barOptions = {
+    responsive: true,
+    plugins: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Transactions Report'
+      }
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+        suggestedMax: barChart?.datasets?.length > 0 && barChart.datasets[0]?.data?.length > 0 ?
+          Math.max(...barChart.datasets[0].data) * 1.1
+          : undefined
+      }
+    }
+  };
+
+
   useEffect(() => {
     walletTransaction()
     banktransaction()
+    barchart()
   }, [transactions])
 
+  const incomeCount = filteredTransaction.filter((item) => item.amount >= 0).length
+  const expenseCount = filteredTransaction.filter((item) => item.amount < 0).length
 
   useEffect(() => {
     settitle('Overview')
@@ -60,13 +148,60 @@ const Overview = () => {
           </div>
         </div>
 
+        <div className='overview-cashflow'>
+          <div>
+            <h3>Cash Flow (Transaction)</h3>
+          </div>
 
+          <table className='cashflow-table'>
+            <thead>
+              <tr >
+                <th>
+                  <div className='dates'>
+                    <button onClick={handleleftarrow}><ChevronLeft /></button>
+                    <p>{formattedMonth}</p>
+                    <button onClick={handlerightarrow}><ChevronRight /></button>
+                  </div>
+                </th>
+                <th>Income</th>
+                <th>Expenses</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td style={{ textAlign: 'start' }}>Total</td>
+                <td>{currencySymbol} {filteredTransaction.reduce((sum, item) => item.amount >= 0 ? sum + (item.amount) : sum, 0)}</td>
+                <td>{currencySymbol} {filteredTransaction.reduce((sum, item) => item.amount < 0 ? sum + (item.amount) : sum, 0)}</td>
+              </tr>
+              <tr>
+                <td style={{ textAlign: 'start' }}>Transaction</td>
+                <td>{incomeCount}</td>
+                <td>{expenseCount}</td>
+              </tr>
+              <tr>
+                <td style={{ textAlign: 'start' }} >Average(Day)</td>
+                <td>{currencySymbol} {(filteredTransaction.reduce((sum, item) => item.amount >= 0 ? sum + (item.amount) : sum, 0) / new Date(selectYear, selectMonth + 1, 0).getDate()).toFixed(2)}</td>
+                <td>{currencySymbol} {(filteredTransaction.reduce((sum, item) => item.amount < 0 ? sum + (item.amount) : sum, 0) / new Date(selectYear, selectMonth + 1, 0).getDate()).toFixed(2)}</td>
 
+              </tr>
+              <tr>
+                <td style={{ textAlign: 'start', borderBottom: "1px solid lightgray" }} >Average (Transactions)</td>
+                <td style={{ borderBottom: "1px solid lightgray" }}>{currencySymbol} {(filteredTransaction.reduce((sum, item) => item.amount >= 0 ? sum + (item.amount) : sum, 0) / incomeCount).toFixed(2)}</td>
+                <td style={{ borderBottom: "1px solid lightgray" }}>{currencySymbol} {(filteredTransaction.reduce((sum, item) => item.amount < 0 ? sum + (item.amount) : sum, 0) / expenseCount).toFixed(2)}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        
+         <div className='overview-BarChart'>
+          <Bar data={barChart} options={barOptions} />
+        </div>
 
         <div className='overview-transaction'>
           <div className='overview-transaction-line'>
             <h3>Transactions</h3>
-            <button onClick={()=> settransactionPopup(!transactionPopup)}><EllipsisVertical size={22} /></button>
+            <button onClick={() => settransactionPopup(!transactionPopup)}><EllipsisVertical size={22} /></button>
 
             {transactionPopup && transactions.length > 0 && (
               <div className='dropdownTransaction'>
@@ -78,7 +213,8 @@ const Overview = () => {
 
                   <div className='content-options'>
                     <p>Number of items shown:</p>
-                    <select onChange={(event)=> setitemShown(event.target.value)}>
+                    <select value={tempitemShown} onChange={(event) => settempitemShown(event.target.value)}>
+                      <option value=""  disabled>Select</option>
                       <option value="3">3</option>
                       <option value="5">5</option>
                       <option value="7">7</option>
@@ -89,11 +225,11 @@ const Overview = () => {
                   </div>
 
                   <div className='content-button'>
-                    <button onClick={()=>settransactionPopup(false)}>CANCEL</button>
-                    <button onClick={()=> settransactionPopup(false)}>SAVE</button>
+                    <button onClick={() => settransactionPopup(false)}>CANCEL</button>
+                    <button onClick={() => (setitemShown(tempitemShown) ,  settransactionPopup(false))}>SAVE</button>
                   </div>
 
-                </div>  
+                </div>
               </div>
             )}
           </div>
@@ -101,7 +237,7 @@ const Overview = () => {
           {transactions.length > 0 ?
             [...transactions]
               .sort((a, b) => new Date(b.date) - new Date(a.date))
-              .slice(0 , itemShown)
+              .slice(0, itemShown)
               .map((item) => (
                 <div key={item._id}>
                   <div className='overview-transaction-line1'>
@@ -115,31 +251,8 @@ const Overview = () => {
                   </div>
                 </div>
               )) : <p> No Transaction Found</p>}
-
-
         </div>
 
-        <div className='overview-lineChart'>
-          <h1>box3</h1>
-        </div>
-
-        <div className='overview-cashflow'>
-          <div>
-            <h3>Cash Flow (Transaction)</h3>
-          </div>
-
-          <div className='cashflow-line1'>
-            <div className='dates'>
-              <button><ChevronLeft /></button>
-              <p>August 2025</p>
-              <button><ChevronRight /></button>
-            </div>
-            <div className='income_expense'>
-              <h3>Income</h3>
-              <h3>Expenses  </h3>
-            </div>
-          </div>
-        </div>
       </div>
 
 
